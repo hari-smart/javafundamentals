b82200ba5d952a6ae8b3e570604b7b1a6a839b15
unknown <madhavihari2504@gmail.com>
2019-11-06 09:47:42 +0530

    Sum added using Eclipse

---
+++ javaFundamentals/src/javaFundamentals/Sum.java
@@ -0,0 +1,14 @@
+package javaFundamentals;
+
+public class Sum {
+
+	public static void main(String[] args) 
+	{
+		int a= 5, b = 15, sum;
+	      sum = a +b;
+
+	      System.out.println("Sum of these numbers: "+sum);
+
+	}
+
+}
